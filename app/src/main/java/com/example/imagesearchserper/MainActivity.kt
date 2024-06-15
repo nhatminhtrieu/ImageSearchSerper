eebcde7f3c9110834eb49add94febc2de21fcbaf
nhatminhtrieu <trieunhatminh2003@gmail.com>
2024-06-15 17:49:05 +0700

    ISS-1: Layout for main screen (including search field and grid layout)

--- app/src/main/java/com/example/imagesearchserper/MainActivity.kt
+++ app/src/main/java/com/example/imagesearchserper/MainActivity.kt
@@ -1,47 +1,24 @@
 package com.example.imagesearchserper
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.imagesearchserper.ui.theme.ImageSearchSerperTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var query: String
+    private lateinit var searchField: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ImageSearchSerperTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+        searchField = findViewById(R.id.searchFieldET)
+
+        searchField.setOnEditorActionListener { _, _, _ ->
+            query = searchField.text.toString()
+            Log.d("MainActivity", "Search query: $query")
+            true
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ImageSearchSerperTheme {
-        Greeting("Android")
     }
 }

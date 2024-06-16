4f9497682027c338bf7441b85ce96f1d8f172f16
nhatminhtrieu <trieunhatminh2003@gmail.com>
2024-06-17 01:05:17 +0700

    ISS-1: card item layout

--- app/src/main/java/com/example/imagesearchserper/view/ImageAdapter.kt
+++ app/src/main/java/com/example/imagesearchserper/view/ImageAdapter.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.core.graphics.drawable.toDrawable
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
